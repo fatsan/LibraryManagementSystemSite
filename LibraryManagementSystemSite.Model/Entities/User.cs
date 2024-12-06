e276a4ec87f09b388daf4d76aa059adf68f5ab0c
Mehmet Fatih SANTIRCI <santircifatih@gmail.com>
2024-12-06 14:15:00 +0300

    Starting to User part

---
+++ LibraryManagementSystemSite.Model/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace LibraryManagementSystemSite.Model.Entities;
+
+public class User // JWT ye bağlayacağım
+{
+    public int DepartmentId { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<Book> BooksCreated { get; set; }
+    public List<Category> CategoriesCreated { get; set; }
+    
+}
--- LibraryManagementSystemSite.WebApi/Program.cs
+++ LibraryManagementSystemSite.WebApi/Program.cs
@@ -1,8 +1,28 @@
+using LibraryManagementSystemSite.Repository.Contexts;
+using LibraryManagementSystemSite.Repository.Repositories.Abstracts;
+using LibraryManagementSystemSite.Repository.Repositories.Concretes;
+using LibraryManagementSystemSite.Service.Abstracts;
+using LibraryManagementSystemSite.Service.Concretes;
+using LibraryManagementSystemSite.Service.Mappings;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IBookRepository, EfBookRepository>();
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICategoryRepository, EfCategoryRepository>();
+
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IAuthorRepository, EfAuthorRepository>();
+
+builder.Services.AddDbContext<BaseDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
